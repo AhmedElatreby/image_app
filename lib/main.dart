13335ad32774cbde7eca33c1110944d66e75ea31
AhmedElatreby <ahmedsport@hotmail.com>
2023-03-29 00:56:37 +0100

    workflows

--- lib/main.dart
+++ lib/main.dart
@@ -4,27 +4,3 @@ import 'src/app.dart';
 void main()  {
   runApp(App());
 }
-
-// import 'dart:convert';
-//
-// void main() {
-//   var rawJson = '{"url": "https://via.placeholder.com/600/92c952", "id": 1}';
-//
-//   var parsedJson = json.decode(rawJson);
-//   var imageModel = new ImageModel.fromJson(parsedJson);
-//
-//   print(imageModel.url);
-// }
-//
-//
-// class ImageModel {
-//   int id=0;
-//   String url="";
-//
-//   ImageModel(this.id, this.url);
-//
-//   ImageModel.fromJson(parsedJson) {
-//     id = parsedJson['id'];
-//     url = parsedJson['url'];
-//   }
-// }
